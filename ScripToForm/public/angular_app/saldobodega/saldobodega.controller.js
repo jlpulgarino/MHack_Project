a1d1b421051aa764775eae30a39c8303680945ed
Le <le.florez602@uniandes.edu.co>
2016-04-02 14:55:02 -0500

    Angular, funcionalidad del maestro saldobodega

--- ScripToForm/public/angular_app/saldobodega/saldobodega.controller.js
+++ ScripToForm/public/angular_app/saldobodega/saldobodega.controller.js
@@ -6,6 +6,36 @@
   saldoBodegaCtrl.$inject = [];
   function saldoBodegaCtrl(){
     var vm = this;
+
+    vm.data = {};
+    vm.data.create = true;
+    vm.data.saldobodegas = [
+      {
+        "id": 1,
+        "nombre": "Bodega 1"
+      },
+      {
+        "id": 2,
+        "nombre": "Bodega 2"
+      }
+    ];
+
+    vm.data.saldobodega = {};
+
+    vm.editSaldoBodega = function(saldobodega){
+      vm.data.create = false;
+      vm.data.saldobodega = saldobodega;
+    };
+
+    vm.addSaldoBodega = function(saldobodega){
+      vm.data.create = true;
+      vm.data.saldobodega = {};
+    };
+
+    vm.removeSaldoBodega = function(saldobodega){
+
+    };
+
   }
 
 })();
