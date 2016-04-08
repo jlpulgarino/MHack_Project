4f6aa83169a79b4b3faf3090ad97dc82750daa31
Le <le.florez602@uniandes.edu.co>
2016-04-08 16:23:54 -0500

    Funcionalidad basica para agregar/editar/borrar bodegas

--- ScripToForm/public/angular_app/bodegas/bodegas.controller.js
+++ ScripToForm/public/angular_app/bodegas/bodegas.controller.js
@@ -3,9 +3,52 @@
   angular.module('invApp')
     .controller('bodegaCtrl', bodegaCtrl);
 
-  bodegaCtrl.$inject = [];
-  function bodegaCtrl(){
+  bodegaCtrl.$inject = ['invData'];
+  function bodegaCtrl(invData){
     var vm = this;
+    vm.data = {};
+
+    invData.getBodegas(function(data){
+      vm.data.bodegas = data;
+    });
+
+    vm.data.create = true;
+    vm.data.bodega = {};
+    vm.addBodega = function(){
+      vm.data.create = true;
+      vm.data.bodega = {};
+    };
+
+    vm.editBodega = function(bodega){
+      vm.data.create = false;
+      vm.data.bodega = {};
+      vm.data.bodega.id = bodega.id;
+      vm.data.bodega.nombre = bodega.nombre;
+    };
+
+    vm.crearBodega = function(){
+      vm.data.create = false;
+      var bodega = angular.copy(vm.data.bodega);
+
+      invData.addBodega(bodega, function(data){
+          vm.data.bodegas.push(bodega);
+      });
+
+      vm.data.bodega = {};
+    };
+
+    vm.deleteBodega = function(id){
+      invData.deleteBodega(id, function(data){
+        for(var i = 0; i < vm.data.bodegas.length; i++){
+          if(vm.data.bodegas[i].id === id){
+            vm.data.bodegas.splice(i, 1);
+            break;
+          }
+        }
+      });
+    };
+
+
   }
 
 })();
--- ScripToForm/public/angular_app/common/services/invData.services.js
+++ ScripToForm/public/angular_app/common/services/invData.services.js
@@ -20,7 +20,38 @@
         ];
     };
 
+
+    var getBodegas = function(callback){
+      $http.get('/api/bodega/').then(function(response){
+        callback(response.data);
+      },
+      function(response){//error
+        console.log(response);
+      });
+    };
+
+    var addBodega = function(data, callback){
+      $http.post('/api/bodega/', data).then(function(response){
+        callback(response.data);
+      },
+      function(response){//error
+        console.log(response);
+      });
+    };
+
+    var deleteBodega = function(id, callback){
+      $http.delete('/api/bodega/' + id).then(function(response){
+        callback(response.data);
+      },
+      function(response){//error
+        console.log(response);
+      });
+    };
+
     return {
+      getBodegas: getBodegas,
+      addBodega: addBodega,
+      deleteBodega: deleteBodega,
       getSaldos: getSaldos
     };
   }
